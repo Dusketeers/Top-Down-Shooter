8f77e2aa5967d6bcb533d6362d8fc566fdc828c3
Ward Bannink <ward.bannink@gmail.com>
2019-09-22 16:46:26 +0200

    Added Enemy Movement
    Enemies will now start to chase the player about. They do so by moving directly towards the player at a steady pace.

---
+++ Assets/Scripts/EnemyScripts/EnemyRushMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRushMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed;
+    private Vector3 dir;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerCharacter");
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(new Vector2(dir.x * speed, dir.y * speed));
+    }
+
+    void Update()
+    {
+        dir = (player.transform.position - transform.position).normalized;
+    }
+}
