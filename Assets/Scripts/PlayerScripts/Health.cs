912091b89033f93a1ff5e032842f314233bd0071
Ward Bannink <ward.bannink@gmail.com>
2019-09-22 15:14:12 +0200

    Fixed typo in Health script

--- Assets/Scripts/PlayerScripts/Health.cs
+++ Assets/Scripts/PlayerScripts/Health.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Heatlh : MonoBehaviour
+public class Health : MonoBehaviour
 {
     public float health;
     public int numOfHearts;
